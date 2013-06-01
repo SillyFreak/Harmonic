bff3773e1771ee658203e0e8121a0fa338837f57
SillyFreak <clemens.koza@gmx.at>
2013-06-01 16:23:25 +0200

    Added code previously written in TicTacToe

---
+++ src/main/java/at/pria/koza/harmonic/Modification.java
@@ -0,0 +1,28 @@
+/**
+ * Modification.java
+ * 
+ * Created on 16.05.2013
+ */
+
+package at.pria.koza.harmonic;
+
+
+/**
+ * <p>
+ * The class Modification.
+ * </p>
+ * 
+ * @version V0.0 16.05.2013
+ * @author SillyFreak
+ */
+public abstract class Modification {
+    public void apply() {
+        //TODO specify exception handling?
+        apply0();
+        Action.get().addModification(this);
+    }
+    
+    public abstract void apply0();
+    
+    public abstract void revert();
+}

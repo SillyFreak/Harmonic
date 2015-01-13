e029232b269de8be8e302edd56e4eeb5544d2983
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-13 16:37:39 +0100

    convert to scala

--- src/main/scala/at/pria/koza/harmonic/Action.scala
+++ src/main/scala/at/pria/koza/harmonic/Action.scala
@@ -13,17 +13,16 @@ import java.util.ListIterator
 
 /**
  * <p>
- * The class {@code Action} represents a high-level change that can happen in the {@link Engine}. An {@code Action}
- * causes one or more {@link Modification Modifications} to happen. These are stored in a list, so they can be
- * {@linkplain #revert() reverted} if necessary.
+ * The class `Action` represents a high-level change that can happen in the `Engine`. An `Action` causes one or
+ * more `Modifications` to happen. These are stored in a list, so they can be reverted if necessary.
  * </p>
  * <p>
- * As an {@code Action} represents an executable, replayable, and even revertable piece of code, calling it is of
- * course delicate; the goal is to get the same result every time it is applied. To achieve this, it is necessary
- * that the original state of the engine is compatible with the action. For a new action, this is the
- * responsibility of the user. For replaying actions on the same or different engines, Harmonic ensures this by
- * associating the action with the same state as the user originally did; together with Harmonic's requirement of
- * determinism of the engine, this should ensure the expected behavior.
+ * As an `Action` represents an executable, replayable, and even revertable piece of code, calling it is of course
+ * delicate; the goal is to get the same result every time it is applied. To achieve this, it is necessary that the
+ * original state of the engine is compatible with the action. For a new action, this is the responsibility of the
+ * user. For replaying actions on the same or different engines, Harmonic ensures this by associating the action
+ * with the same state as the user originally did; together with Harmonic's requirement of determinism of the
+ * engine, this should ensure the expected behavior.
  * </p>
  *
  * @version V1.0 26.07.2013
@@ -81,10 +80,10 @@ abstract class Action(engine: Engine) {
 
   /**
    * <p>
-   * Applies the action to the engine. This method should only be called by {@link Engine#setHead(State)}. This
-   * method invokes {@link #apply0()}, surrounded by calls to {@link #push(Action)}, and {@link #pop(Action)} in
-   * a {@code finally} block to always leave the action stack in a well defined way. Note that this does not mean
-   * that the engine, too, will be in a defined state; see {@link #apply0()}.
+   * Applies the action to the engine. This method should only be called by `Engine.setHead(State)`. This  method
+   * invokes `apply0()`, surrounded by calls to `push(Action)`, and `pop(Action)` in a `finally` block to always
+   * leave the action stack in a well defined way. Note that this does not mean that the engine, too, will be in a
+   * defined state; see `apply0()`.
    * </p>
    */
   def apply(): Unit = {
@@ -98,9 +97,9 @@ abstract class Action(engine: Engine) {
 
   /**
    * <p>
-   * Reverts the action by {@linkplain Modification#revert() reverting} every {@link Modification} in reverse
-   * order. This method must only be called by the {@link Engine}. When this method is called, the engine will be
-   * in the same state as it was after {@linkplain #apply() applying} this action.
+   * Reverts the action by reverting every `Modification` in reverse order. This method must only be called by the
+   * `Engine`. When this method is called, the engine will be in the same state as it was after applying this
+   * action.
    * </p>
    */
   def revert(): Unit = {
@@ -113,11 +112,11 @@ abstract class Action(engine: Engine) {
 
   /**
    * <p>
-   * Called by {@link Modification#addToAction()}. Adds a {@link Modification} to this action so that it can be
-   * subsequently reverted.
+   * Called by `Modification.addToAction()`. Adds a `Modification` to this action so that it can be subsequently
+   * reverted.
    * </p>
    *
-   * @param m the {@link Modification} to add
+   * @param m the `Modification` to add
    */
   def addModification(m: Modification): Unit = {
     _modifications.add(m)
@@ -125,14 +124,13 @@ abstract class Action(engine: Engine) {
 
   /**
    * <p>
-   * Performs the actual action. This method must only be called from {@link #apply()}, where it will have an
-   * {@code Action} context established for {@link Modification}s via the {@link #get()} method.
+   * Performs the actual action. This method must only be called from `apply()`, where it will have an `Action`
+   * context established for `Modification`s via the `get()` method.
    * </p>
    * <p>
-   * Note that this method is responsible for leaving the {@link Engine} in a well-defined state. If it does not
-   * return normally, and the application needs to be able to roll back the engine, then this method must leave
-   * the engine in a state where {@linkplain #revert() reverting} this action really does revert it into the
-   * previous state.
+   * Note that this method is responsible for leaving the `Engine` in a well-defined state. If it does not return
+   * normally, and the application needs to be able to roll back the engine, then this method must leave the engine
+   * in a state where reverting this action really does revert it into the previous state.
    * </p>
    */
   def apply0(): Unit

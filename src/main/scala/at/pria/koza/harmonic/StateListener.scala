21d48d62fe900c8dbed4d2a6f93b0322a4327cc6
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-13 15:43:14 +0100

    convert to scala

---
+++ src/main/scala/at/pria/koza/harmonic/StateListener.scala
@@ -0,0 +1,30 @@
+/**
+ * StateListener.scala
+ *
+ * Created on 07.08.2013
+ */
+
+package at.pria.koza.harmonic
+
+import java.util.EventListener
+
+/**
+ * <p>
+ * `StateListener` provides a listener interface that can be used to detect when new states are added to an engine.
+ * </p>
+ *
+ * @version V1.0 07.08.2013
+ * @author SillyFreak
+ */
+trait StateListener extends EventListener {
+  /**
+   * <p>
+   * Called when a `State` is added. All relevant information can be retrieved from the state: The engine the state
+   * was added to via `getEngine()`; the engine originating the state via `getEngineId()` etc. Note that, because
+   * at this moment the state was definitely not executed yet, `getAction()` will return `null`.
+   * </p>
+   *
+   * @param state the state that was added
+   */
+  def stateAdded(state: State): Unit
+}

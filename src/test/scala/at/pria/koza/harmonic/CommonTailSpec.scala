d47c67e7518b33f4d6426a1e63bf43269bbf0391
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-17 00:39:08 +0100

    added another test case

--- src/test/scala/at/pria/koza/harmonic/CommonTailSpec.scala
+++ src/test/scala/at/pria/koza/harmonic/CommonTailSpec.scala
@@ -19,11 +19,13 @@ import org.scalatest.Matchers
 class CommonTailSpec extends FlatSpec with Matchers {
   behavior of "commonTail"
 
-  it should "return Nil when first list is Nil" in {
+  it should "return Nil when either list is Nil" in {
     State.commonTail(Nil, Stream.from(0)) should be(Nil)
+    State.commonTail(Stream.from(0), Nil) should be(Nil)
+    State.commonTail(Nil, Nil) should be(Nil)
   }
 
-  it should "return Nil when second list is Nil" in {
-    State.commonTail(Stream.from(0), Nil) should be(Nil)
+  it should "return the original list for two equal lists" in {
+    State.commonTail("abcd", "abcd") should be("abcd": Seq[Char])
   }
 }

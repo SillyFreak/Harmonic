74144d038bf5088f3f29ef5ee0b827fc9f117684
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-17 12:04:37 +0100

    more tests

--- src/test/scala/at/pria/koza/harmonic/CommonTailSpec.scala
+++ src/test/scala/at/pria/koza/harmonic/CommonTailSpec.scala
@@ -49,4 +49,53 @@ class CommonTailSpec extends FlatSpec with Matchers {
 
     State.commonTail(list1, list2) should be(list1.drop(4))
   }
+
+  it should "return the original sequence for two equal nonlist sequences" in {
+    val list = "1234": Seq[Char]
+
+    State.commonTail(list, list) should be(list)
+  }
+
+  it should "return the common tail for two different nonlist sequences of equal length" in {
+    val list1 = "123456": Seq[Char]
+    val list2 = "789456": Seq[Char]
+
+    State.commonTail(list1, list2) should be(list1.drop(3))
+  }
+
+  it should "return the common tail for two different nonlist sequences of different lengths" in {
+    val list1 = "0123456": Seq[Char]
+    val list2 = "9456": Seq[Char]
+
+    State.commonTail(list1, list2) should be(list1.drop(4))
+  }
+
+  it should "return the original sequence for two equal infinite sequences" in {
+    val list = Stream.from(0)
+
+    State.commonTail(list, list) should be(list)
+  }
+
+  it should "return the common tail for two different infinite sequences where one is a subsequence of the other" in {
+    val list1 = Stream.from(2)
+    val list2 = Stream.from(0)
+
+    State.commonTail(list1, list2) should be(list1)
+  }
+
+  it should "return the common tail for two different infinite sequences with diverging heads of same length" in {
+    val tail = Stream.from(0)
+    val list1 = Stream.cons(-1, tail)
+    val list2 = Stream.cons(-2, tail)
+
+    State.commonTail(list1, list2) should be(tail)
+  }
+
+  it should "return the common tail for two different infinite sequences with diverging heads of different lengths" in {
+    val tail = Stream.from(0)
+    val list1 = Stream.cons(-5, Stream.cons(-3, Stream.cons(-1, tail)))
+    val list2 = Stream.cons(-2, tail)
+
+    State.commonTail(list1, list2) should be(tail)
+  }
 }

1d1c87c8744316e596d09869f57c95a538b79207
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-24 17:40:21 +0100

    added test case

--- src/test/scala/at/pria/koza/harmonic/StateWrapperSpec.scala
+++ src/test/scala/at/pria/koza/harmonic/StateWrapperSpec.scala
@@ -29,4 +29,18 @@ class StateWrapperSpec extends FlatSpec with Matchers with GivenWhenThen {
 
     engine.wrappers(0l).state should be(engine.head())
   }
+
+  it should "be resolvable if it's wrapping a known derived state" in {
+    Given("a new engine")
+    implicit val engine = new Engine()
+
+    And("a PolybufIO for a custom action")
+    engine.addIO(MyAction)
+
+    When("executing an action")
+    val action = engine.execute(new MyAction())
+
+    Then("the new head's wrapper should be resolvable")
+    engine.wrappers(engine.head().id).state should be(engine.head())
+  }
 }

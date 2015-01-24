70d7b5ce566fee25eb47898814bba9c8bfeb48c1
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-01-24 18:39:26 +0100

    add test case

--- src/test/scala/at/pria/koza/harmonic/StateWrapperSpec.scala
+++ src/test/scala/at/pria/koza/harmonic/StateWrapperSpec.scala
@@ -8,6 +8,9 @@ package at.pria.koza.harmonic
 
 import scala.collection.mutable
 
+import at.pria.koza.polybuf.PolybufOutput
+import at.pria.koza.polybuf.PolybufInput
+
 import org.scalatest.FlatSpec
 import org.scalatest.GivenWhenThen
 import org.scalatest.Matchers
@@ -27,7 +30,7 @@ class StateWrapperSpec extends FlatSpec with Matchers with GivenWhenThen {
     implicit val engine = new Engine()
     engine.addIO(StateWrapper)
 
-    engine.wrappers(0l).state should be(engine.head())
+    engine.wrappers.head.state should be(engine.head())
   }
 
   it should "be resolvable if it's wrapping a known derived state" in {
@@ -41,6 +44,41 @@ class StateWrapperSpec extends FlatSpec with Matchers with GivenWhenThen {
     val action = engine.execute(new MyAction())
 
     Then("the new head's wrapper should be resolvable")
-    engine.wrappers(engine.head().id).state should be(engine.head())
+    engine.wrappers.head.state should be(engine.head())
+  }
+
+  it should "be resolvable in another engine if the parent is known" in {
+    val (obj, id) = {
+      Given("one engine with IOs for StateWrapper and MyAction")
+      implicit val engine = new Engine()
+      engine.addIO(StateWrapper)
+      engine.addIO(MyAction)
+
+      And("executing an action")
+      engine.execute(new MyAction())
+      val wrapper = engine.wrappers.head
+
+      And("serializing the head state")
+      val out = new PolybufOutput(engine.config)
+      (out.writeObject(wrapper), wrapper.stateId)
+    }
+
+    {
+      When("creating a new engine with IOs for StateWrapper and MyAction")
+      implicit val engine = new Engine()
+      engine.addIO(StateWrapper)
+      engine.addIO(MyAction)
+
+      And("deserializing the head in that engine")
+      val in = new PolybufInput(engine.config)
+      in.readObject(obj)
+
+      Then("the wrapper should be available")
+      val wrapper = engine.wrappers.get(id)
+      wrapper should not be None
+
+      And("it should be resolvable")
+      wrapper.get.state should be(engine.states(id))
+    }
   }
 }

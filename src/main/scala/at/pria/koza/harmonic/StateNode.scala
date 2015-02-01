0343ce187c1c58fd3080c7cb807334b62b30e85a
Clemens Koza <clemens@clemens-len-xubuntu14.04>
2015-02-01 20:02:40 +0100

    StateNode Scaladoc

--- src/main/scala/at/pria/koza/harmonic/StateNode.scala
+++ src/main/scala/at/pria/koza/harmonic/StateNode.scala
@@ -1,7 +1,7 @@
 /**
  * StateNode.scala
  *
- * Created on 16.05.2013
+ * Created on 31.01.2015
  */
 
 package at.pria.koza.harmonic
@@ -22,10 +22,10 @@ import com.google.protobuf.GeneratedMessage.GeneratedExtension
 
 /**
  * <p>
- * The class StateNode.
+ * The `StateNode` object provides an `IOFactory` to serialize `StateNode`.
  * </p>
  *
- * @version V0.0 16.05.2013
+ * @version V1.0 31.01.2015
  * @author SillyFreak
  */
 object StateNode extends IOFactory[StateNode] {
@@ -63,10 +63,25 @@ object StateNode extends IOFactory[StateNode] {
   }
 }
 
+/**
+ * <p>
+ * The `StateNode` class represents one node in an engine's history. It is typically accessed through `State`,
+ * which also allows accessing the parent state directly, while StateNode only exposes the parent's ID. Informally,
+ * "state" can refer to both a `State` and a `StateNode`.
+ * </p>
+ *
+ * @version V1.0 31.01.2015
+ * @author SillyFreak
+ */
 case class StateNode private[harmonic] (val id: Long, val parentId: Long, val action: Obj) extends PolybufSerializable {
   //PolybufSerializable
   override def typeId: Int = StateNode.FIELD
 
+  /**
+   * Creates a StateNode using an `Action` object. The implicit engine must be able to serialize the action. The
+   * given action is usually discarded: even if the state is applied directly after creation, a new action is
+   * freshly deserialized for that purpose.
+   */
   def this(id: Long, parentId: Long, action: Action)(implicit engine: Engine) =
     this(
       id,
@@ -78,8 +93,17 @@ case class StateNode private[harmonic] (val id: Long, val parentId: Long, val ac
           "can't serialize action; try registering the IOFactory", ex);
       })
 
+  /**
+   * The ID of the engine that originally created this state. This is simply the upper 32 bits of the state
+   * id.
+   */
   def engineId: Int = (id >> 32).toInt
 
+  /**
+   * deserializes the state's action using the implicit engine's `PolybufConfig`. As the action may reference
+   * entities in the engine, this method may fail if the engine is in a state other than before executing the
+   * stored action.
+   */
   def deserializedAction(implicit engine: Engine): Action =
     try {
       new PolybufInput(engine.config).readObject(action).asInstanceOf[Action]
